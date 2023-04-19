616e3c0cb0485dc54938d40409081e8e08eb24cc
RaulRivera15 <123921784+RaulRivera1515@users.noreply.github.com>
2023-04-19 00:02:41 -0600

    categoria
    .

--- Ferrecomm/accesos_usuarios/administrador/categoria.php
+++ Ferrecomm/accesos_usuarios/administrador/categoria.php
@@ -91,7 +91,7 @@ if(!isset ($_SESSION['usuario'])){
             <span class="tooltip">Proveedores</span>
         </li>
         <li>
-            <a href="../Inventario.php">
+            <a href="./Inventario.php">
                 <i class='bx bx-package'></i>
                 <span class="links_name">Inventario</span>
             </a>
