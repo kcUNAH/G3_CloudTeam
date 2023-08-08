a6e06ffb3850fc09651d692c9fa25c0c93570ab6
RaulRivera15 <123921784+RaulRivera1515@users.noreply.github.com>
2023-08-07 22:16:57 -0600

    Página no encontrada
    Solución a error 404 sobre ubicación o página no encontrada

---
+++ 404.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página no encontrada</title>
+    <link rel="stylesheet" href="Ferrecomm/pagina504/estillos404.css">
+    <style>
+    </style>
+</head>
+<body>
+<header>
+		<nav class="navegacion">
+			<ul class="menu">
+				<li><a href="Ferrecomm/index.php">Iniciar sesión</a></li>
+			</ul>
+		</nav>
+	</header>
+
+
+    </br>
+    </br>
+    </br>
+    <h1>Página no encontrada</h1>
+    <center>
+        <img width="220" src="Ferrecomm/pagina504/Error404.png" alt="">
+    </center>    </br>
+
+    <div>
+    <a href="javascript:history.back()" class='btn'>Regresar</a>
+    </div>
+    
+</body>
+</html>
