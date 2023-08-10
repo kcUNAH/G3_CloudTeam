da9a6bbeb178b4e7de7b3186723e62073a46555e
RaulRivera15 <123921784+RaulRivera1515@users.noreply.github.com>
2023-08-09 23:31:26 -0600

    Registro en bitácora (salidas)
    Se hicieron los registros en bitácora sobre la generación de reportes en descuentos, productos, promoción, categorías y preguntas

--- Ferrecomm/fpdf/Reportepreguntasbuscar.php
+++ Ferrecomm/fpdf/Reportepreguntasbuscar.php
@@ -1,4 +1,19 @@
 <?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    echo '
+    <script>
+    alert("Por favor, debe iniciar seccion");
+    window.location= "index.php";
+    </script>
+    ';
+    //header("localitation: index.php");
+    session_destroy();
+    die();
+}
+?>
+
+<?php
 
 require('./fpdf.php');
 
@@ -95,6 +110,12 @@ class PDF extends FPDF
 }
 
 include '../php/conexion.php';
+include '../php/bitacora.php';
+
+$codigoObjeto=7;
+$accion='Generó reporte';
+$descripcion= 'El usuario generó un reporte de categorías';
+bitacora($codigoObjeto, $accion,$descripcion);
 
 $pdf = new PDF();
 $pdf->AddPage("landscape"); /* aqui entran dos para parametros (horientazion,tamaño)V->portrait H->landscape tamaño (A3.A4.A5.letter.legal) */
