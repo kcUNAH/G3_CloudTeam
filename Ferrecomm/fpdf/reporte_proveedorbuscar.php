f74550c131b16bd556761530899e18ca42babe00
RaulRivera15 <123921784+RaulRivera1515@users.noreply.github.com>
2023-08-11 23:06:11 -0600

    Reporte
    .

--- Ferrecomm/fpdf/reporte_proveedorbuscar.php
+++ Ferrecomm/fpdf/reporte_proveedorbuscar.php
@@ -1,4 +1,19 @@
 <?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    echo '
+    <script>
+    alert("Por favor, debe iniciar seccion");
+    window.location= "index.php";
+    </script>
+    ';
+    //header("localitation: index.php");
+    session_destroy();
+    die();
+}
+?>
+
+<?php
 
 require('./fpdf.php');
 
@@ -66,7 +81,7 @@ class PDF extends FPDF
       $this->SetTextColor(228, 100, 0);
       $this->Cell(90); // mover a la derecha
       $this->SetFont('Arial', 'B', 15);
-      $this->Cell(100, 10, utf8_decode("Reporte de productos "), 0, 1, 'C', 0);
+      $this->Cell(100, 10, utf8_decode("REPORTE DE PROVEEDORES"), 0, 1, 'C', 0);
       $this->Ln(7);
 
       /* CAMPOS DE LA TABLA */
@@ -75,13 +90,13 @@ class PDF extends FPDF
       $this->SetTextColor(255, 255, 255); //colorTexto
       $this->SetDrawColor(163, 163, 163); //colorBorde
       $this->SetFont('Arial', 'B', 11);
-      $this->Cell(70,20, utf8_decode('nombre_proveedor'), 1, 0, 'C', 1);
-      $this->Cell(60, 20, utf8_decode('rtn_proveedor'), 1, 0, 'C', 1);
+      $this->Cell(70,20, utf8_decode('Nombre proveedor'), 1, 0, 'C', 1);
+      $this->Cell(60, 20, utf8_decode('Rtn'), 1, 0, 'C', 1);
      // $this->Cell(40, 10, utf8_decode('Descripcion'), 1, 0, 'C', 1);
-      $this->Cell(50, 20, utf8_decode('telefono_proveedor'), 1, 0, 'C', 1);
+      $this->Cell(50, 20, utf8_decode('Teléfono'), 1, 0, 'C', 1);
       //$this->Cell(30, 10, utf8_decode('Imagen'), 1, 0, 'C', 1);
-      $this->Cell(50, 20, utf8_decode('correo_proveedor'), 1, 0, 'C', 1);
-      $this->Cell(50, 20, utf8_decode('direccion_proveedor'), 1, 1, 'C', 1);
+      $this->Cell(50, 20, utf8_decode('Correo'), 1, 0, 'C', 1);
+      $this->Cell(50, 20, utf8_decode('Dirección'), 1, 1, 'C', 1);
                                         
    }
 
@@ -100,6 +115,12 @@ class PDF extends FPDF
 }
 
 include '../php/conexion.php';
+include '../php/bitacora.php';
+
+$codigoObjeto=7;
+$accion='Generó reporte';
+$descripcion= 'El usuario generó un reporte de proveedores';
+bitacora($codigoObjeto, $accion,$descripcion);
 
 $pdf = new PDF();
 $pdf->AddPage("landscape"); /* aqui entran dos para parametros (horientazion,tamaño)V->portrait H->landscape tamaño (A3.A4.A5.letter.legal) */
